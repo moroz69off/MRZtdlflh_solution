bdb1e5fb79e0c64329d9e42bbf7f1bb1ddae3c9f
moroz69off <moroz69off@gmail.com>
2021-01-15 00:08:21 +0300

    tdclient ok

--- MRZtdlflh_pro/Program.cs
+++ MRZtdlflh_pro/Program.cs
@@ -5,17 +5,34 @@ using TDLib.Bindings;
 
 using TdLib;
 using TdLib.Bindings;
-
+using System.Threading.Tasks;
+using static TdLib.TdApi;
 
 namespace MRZtdlflh_pro
 {
     class Program
     {
+        private static string phoneNumber;
+
         static void Main(string[] args)
         {
-            TdJsonClient tdJsonClient = new TdJsonClient();
+            try
+            {
+                Task<TdClient> v = TaskAsync();
+                v.Dispose();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
             //Console.WriteLine("Hello World!");
-            //Console.ReadLine();
+            Console.ReadLine();
+        }
+        static async Task<TdClient> TaskAsync()
+        {
+            TdClient tdClient = new TdClient();
+            var GetMeClient = await TdApi.GetMeAsync(tdClient);
+            return tdClient;
         }
     }
 }
