4e91ebb08d4b94912672ef27ea1acb4acfb74f18
moroz69off <moroz69off@gmail.com>
2021-01-15 18:49:53 +0300

    td autorisation result

--- MTDvsFH/Program.cs
+++ MTDvsFH/Program.cs
@@ -1,16 +1,39 @@
 ﻿using System;
+using System.Threading.Tasks;
 using TdLib;
-
+using TdA = TdLib.TdApi;
 
 namespace MTDvsFH
 {
     class Program
     {
+        private static string phoneNumber;
+        static TdA.Ok Tok;
+        private static TdA.Client client;
+        static TdA.InlineQueryResult GetInlineQueryResult = null;
+        private static TdA.TdlibParameters parameters;
+
         static void Main(string[] args)
         {
-            TdClient MtdClient = new TdClient();
+            Console.WriteLine("Type the you phone number");
+            //phoneNumber = Console.ReadLine();
 
-            MtdClient.Dispose();
+            parameters = new TdA.TdlibParameters()
+            {
+                ApiId = int.Parse(mResource.apiId),
+                ApiHash = mResource.apiHash,
+                ApplicationVersion = "1.0",
+                SystemLanguageCode = "ru-RU",
+                DeviceModel = "desktop",
+                SystemVersion = "Windows 10 Pro"
+            };
+
+            client = new TdClient();
+            client.SetTdlibParametersAsync(parameters);
+            client.CheckDatabaseEncryptionKeyAsync();
+            TaskStatus authorizationStateStatus = client.GetAuthorizationStateAsync().Status;
+            Console.WriteLine("===authorization state status===:\n" + authorizationStateStatus + "\n===ЖЖЖЖЖЖЖЖЖЖЖЖЖЖ===\n");
+            client.GetAuthorizationStateAsync().Start();
             Console.ReadLine();
         }
     }
