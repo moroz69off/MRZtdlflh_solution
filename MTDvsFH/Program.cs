8142499c590a5060e0f508f099d70fd27d74b32f
moroz69off <moroz69off@gmail.com>
2021-01-17 01:07:18 +0300

    closed client

--- MTDvsFH/Program.cs
+++ MTDvsFH/Program.cs
@@ -2,27 +2,32 @@
 using System.Threading.Tasks;
 using TdLib;
 using TdAPI = TdLib.TdApi;
+using JsonSerial = Newtonsoft.Json.Serialization;
+using System.Text;
 
 namespace MTDvsFH
 {
     class Program
     {
-        private static string phoneNumber;
-        static TdAPI.Ok Tok;
-        private static TdAPI.Client client;
+        private static byte[] encryptionKey = null;
+        private static string phoneNumber = null;
+        static TdAPI.Ok Tok = null;
+        private static TdAPI.Client client = null;
         static TdAPI.InlineQueryResult GetInlineQueryResult = null;
         private static TdAPI.TdlibParameters parameters;
 
         static void Main(string[] args)
         {
             Console.Title = "moroz69off tg-client";
-
-            Console.WriteLine("Type the you phone number");
+            //encryptionKey = Encoding.UTF8.GetBytes("MIIBCgKCAQEAwVACPi9w23mF3tBkdZz+zwrzKOaaQdr01vAbU4E1pvkfj4sqDsm6");
+            //Console.WriteLine("Type the you phone number");
             //phoneNumber = Console.ReadLine();
 
             parameters = new TdAPI.TdlibParameters()
             {
                 ApiId = int.Parse(mResource.apiId),
+                UseChatInfoDatabase=true,
+                UseMessageDatabase=true,
                 ApiHash = mResource.apiHash,
                 ApplicationVersion = "1.0",
                 SystemLanguageCode = "ru-RU",
@@ -32,11 +37,17 @@ namespace MTDvsFH
                 UseTestDc = true
             };
 
-            client = new TdClient();
+            client = new TdLib.TdClient();
             client.SetTdlibParametersAsync(parameters);
-            client.CheckDatabaseEncryptionKeyAsync();
+            //client.CheckDatabaseEncryptionKeyAsync(encryptionKey);
 
-            Task<TdAPI.Chat> mChat = client.GetChatAsync(464756882);
+            //client = new TdClient();
+            //client.SetTdlibParametersAsync(parameters);
+            //client.CheckDatabaseEncryptionKeyAsync();
+
+            //Task<TdAPI.Chat> mChat = client.GetChatAsync(464756882);
+
+            client.CloseAsync();
 
             Console.ReadLine();
         }
